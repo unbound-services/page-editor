d7a96abb51068e16ab37887528effcec602614af
Radet 5 <r.5@unbound.services>
2022-05-03 11:22:45 -0400

    copy over styling

--- src/lib/modules/page-meta/page-meta.tsx
+++ src/lib/modules/page-meta/page-meta.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import "./page-meta.scss";
+
 export const PageMeta = ({pageMeta, updatePageMetaState}) => {
 
   const onChange = (e, key) => {
@@ -7,23 +9,23 @@ export const PageMeta = ({pageMeta, updatePageMetaState}) => {
   };
 
   return (
-    <div className='page-editor__drawer-contents'>
-      <div className='pe-drawer-contents__option'>
+    <div className='drawer-contents'>
+      <div className='drawer-contents__option'>
           <label>Page Name:<br />
-              <input name='name' onChange={(e) => onChange(e, "name")} value={pageMeta.name} />
+              <input className="drawer-contents__input" name='name' onChange={(e) => onChange(e, "name")} value={pageMeta.name} />
           </label>
       </div>
-      <div className='pe-drawer-contents__option'>
+      <div className='drawer-contents__option'>
           <label>Page Status (drafts are only visible to admins):
-                  <select name="status" className="base-form__select" onChange={(e) => onChange(e, "status")} value={pageMeta.status}>
+                  <select name="status" className="base-form__select drawer-contents__input" onChange={(e) => onChange(e, "status")} value={pageMeta.status}>
                           <option value="draft">Draft</option>
                           <option value="publish">Publish</option>
                   </select>
           </label>
       </div>
-      <div className='pe-drawer-contents__option'>
+      <div className='drawer-contents__option'>
           <label>Page Url:<br />
-              <input name='slug' onChange={(e) => onChange(e, "slug")} value={pageMeta.slug} />
+              <input className="drawer-contents__input" name='slug' onChange={(e) => onChange(e, "slug")} value={pageMeta.slug} />
               <div style={{fontSize:"0.8em", fontColor:"#dddddd", paddingTop:4}}>Set to "/" to make this homepage</div>
           </label>
       </div>
